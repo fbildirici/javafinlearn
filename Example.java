1f165647ca4add327a756fb8b1799af189a2d120
fbildirici <fatihbildirici.dev@gmail.com>
2023-02-01 12:27:47 +0300

    first program

--- Example.java
+++ Example.java
@@ -1,6 +1,6 @@
 public class Example {
     public static void main(String[] args) {
         // The statements used by the program are placed here
-        System.out.println("Welcome Aboard - This is Firts Code of Java Learning HVLFB");
+        System.out.println("Welcome, Aboard it's First Java Learning code HVLFB!");
     }
 }
