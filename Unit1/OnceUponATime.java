c65a80aa45184b6db5d2b0e478bb41428dca330a
fbildirici <fatihbildirici.dev@gmail.com>
2023-02-01 14:24:33 +0300

    onceuponatimeexample

---
+++ Unit1/OnceUponATime.java
@@ -0,0 +1,10 @@
+public class OnceUponATime {
+    public static void main(String[] args) {
+        // Write your program here
+System.out.println("Once Upon A Time");
+System.out.println("There was a Software Researcher");
+System.out.println("In HAVELSAN");
+System.out.println("Who works in technical agility");
+
+    }
+}
